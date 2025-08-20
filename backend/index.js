c708097d8e869ec50d55064b0012f4a31a717b45
Ayan Manna <ayanmanna858@gmail.com>
2025-08-20 18:42:09 +0530

    route update

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { auth } from "express-openid-connect";
+import pkg from "express-openid-connect";
+const { auth, requiresAuth } = pkg;
+
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import connectToMongo from "./utils/db.js";
-import fs from "fs";
-
 import asyncHandler from "express-async-handler";
 import User from "./models/User.model.js";
 import router from "./routes/auth.route.js";
@@ -32,6 +32,7 @@ const config = {
   clientID: process.env.CLIENT_ID,
   issuerBaseURL: process.env.ISSUER_BASE_URL,
 };
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -43,17 +44,13 @@ const enusureUserInDB = asyncHandler(async (user) => {
     const existingUser = await User.findOne({ auth0Id: user.sub });
 
     if (!existingUser) {
-      // create a new user document
       const newUser = new User({
         auth0Id: user.sub,
         email: user.email,
         name: user.name,
-
         profilePicture: user.picture,
       });
-
       await newUser.save();
-
       console.log("User added to db", user);
     } else {
       console.log("User already exists in db", existingUser);
@@ -62,24 +59,21 @@ const enusureUserInDB = asyncHandler(async (user) => {
     console.log("Error checking or adding user to db", error.message);
   }
 });
+
 app.get("/chake", async (req, res) => {
   if (req.oidc.isAuthenticated()) {
-    // check if Auth0 user exists in the db
     await enusureUserInDB(req.oidc.user);
-
-    // redirect to the frontend
     return res.redirect(process.env.CLIENT_URL);
   } else {
     return res.redirect(process.env.CLIENT_URL);
   }
 });
 
-app.use("/api/v1/user", router);
+app.use("/api/v1/user", requiresAuth(), router);
 
 const server = async () => {
   try {
     await connectToMongo();
-
     app.listen(process.env.PORT, () => {
       console.log(`Server is running on port ${process.env.PORT}`);
     });
